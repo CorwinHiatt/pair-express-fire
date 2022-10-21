b031e1cc502637d5e34b4ca73f6d60dbc2b1f4bc
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-21 14:41:09 -0400

    done Getted  through postman from firestore and local host successfully

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { createClothing } from './src/clothing.js'
+import { createClothing, getClothing } from './src/clothing.js'
 
 const app = express()
-
 app.use(express.json())
+
 app.post('/clothing', createClothing)
 app.listen(3035, () => console.log('Listening on http://localhost:3035... '))
+
+app.get('/clothing', getClothing)
--- src/clothing.js
+++ src/clothing.js
@@ -4,13 +4,21 @@ import dbConnect from "./dbConnect.js"
 
 
 
-export function createClothing(req, res) {
-    
-    const db = dbConnect()
-    
-    db.collection('clothing').add(req.body)
-    
+export function createClothing(req, res) { 
+      const db = dbConnect()
+      db.collection('clothing').add(req.body)
         .then(doc => res.status(201).send({ success: true, message: 'Created clothing: ' + doc.id }))
         .catch(err => res.send.status(500).send({success: false, message: err}))
 
 }
+export function getClothing(req, res) { 
+      const db = dbConnect()
+      db.collection('clothing').get()
+        .then(collection => {
+            const clothingList = collection.docs.map (doc => doc.data())
+            res.send(clothingList)
+        })
+    
+        .catch(err => res.send.status(500).send({success: false, message: err}))
+}
+
